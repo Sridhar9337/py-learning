6a59102c67bef5593cc90b905b8510d1da3cf388
Sridhar9337 <sspn9337@gmail.com>
2025-03-15 10:20:47 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,23 @@
+print("sridhar")
+A="Rohit"
+B="Sharma"
+c=A+" "+B
+print(c)
+print(c.upper())
+#All lower to caps
+print(c.lower())
+#All caps to lower
+print(c.swapcase())
+#use to swap all caps to small and small to caps
+print(c.title())
+#use to change the first letter to caps
+print(c.find('Sha'))
+#to find the starting index of the letter,from first apperence
+print(c.replace('Rohit','ishant'))
+#to replace the name
+print(c[6:8]) #Slicing
+#to slice the character
+print(c[-2]) #indexing
+#to give the exact position
+print(c.strip())
+
